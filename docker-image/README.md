e1c832cbecb0c6a9ae47af8085e2db909003f09b
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2021-09-20 23:43:10 +0200

    Initial commit

---
+++ docker-image/README.md
@@ -0,0 +1,8 @@
+# Push helm charts to Artifactory
+
+- a docker image to push Helm charts to Artifactory
+
+## Arguments
+
+
+## Checks
---
+++ docker-image/push_to_artifactory.sh
@@ -0,0 +1,4 @@
+#!/bin/ash
+
+
+helm push-artifactory /charts/helm/ https://artifactory.internal.sysdig.com:443/artifactory/helm-local/ --username ${ARTIFACTORY_USER} --password ${ARTIFACTORY_PASSWORD} --version ${CHART_VERSION}
