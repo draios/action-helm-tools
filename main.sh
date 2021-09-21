8b3c67b45f99b0264c5c54d9947f181b0a9f203c
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2021-09-21 16:27:09 +0200

    Work on changes to script

--- main.sh
+++ main.sh
@@ -3,16 +3,38 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-
 SCRIPT_DIR=$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}" || realpath "${BASH_SOURCE[0]}")")
 export SCRIPT_DIR
 source "$SCRIPT_DIR/common.sh"
 
+# optional arguments
+CHART_VERSION_EXT=${CHART_VERSION_EXT:-}
+WAIT_FOR_CHART=${WAIT_FOR_CHART:-"false"}
+HELM_REPO_URL=${HELM_REPO_URL:-}
+ACTION=${ACTION:-"all"}
+
+### main ##############
 install_helm
 install_artifactory_plugin
-get_chart_version
+if [[ -z "${CHART_VERSION_EXT}" ]]; then
+    get_chart_version
+else
+    logit "INFO" "using CHART_VERSION: ${CHART_VERSION_EXT}"
+    CHART_VERSION=${CHART_VERSION_EXT}
+fi
 
 case "${ACTION}" in
+    "all")
+        logit "INFO" "action is all"
+        print_title "Helm dependency build"
+        helm dependency build "${CHART_DIR}"
+        print_title "Linting"
+        helm lint "${CHART_DIR}"
+        print_title "Helm package"
+        helm package "${CHART_DIR}" --version v"${CHART_VERSION}" --app-version "${CHART_VERSION}" --destination "${RUNNER_WORKSPACE}"
+        print_title "Push chart"
+        helm push-artifactory "${CHART_DIR}" "${ARTIFACTORY_URL}" --username "${ARTIFACTORY_USERNAME}" --password "${ARTIFACTORY_PASSWORD}" --version "${CHART_VERSION}"
+        ;;
     "package")
         print_title "Helm dependency build"
         helm dependency build "${CHART_DIR}"
